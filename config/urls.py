071fc39ebaf48a38848c5f63d9c1be509954e231
jpwilson <jpwilson@hey.com>
2021-04-27 23:45:49 -0500

    ch10 mid - add booklist, bookdetail

--- config/urls.py
+++ config/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.contrib import admin 
 from django.urls import path, include
 
 urlpatterns = [
@@ -25,5 +25,6 @@ urlpatterns = [
 
     # Local apps
     path('accounts/', include('accounts.urls')),
-    path('', include('pages.urls'))
+    path('', include('pages.urls')),
+    path('books/', include('books.urls')),
 ]
